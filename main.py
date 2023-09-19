70c0b3f0381cc0d192a128d9129e32b20ce3ceef
jon-edward <arithmatlic@gmail.com>
2023-09-19 14:08:40 -0400

    Add CLI arguments --force-update, --suppress-logging. Remove start, end

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import logging
 import pathlib
 from typing import Optional
 
-from category_tree.scripts.update import update_all
+from category_tree.scripts.update import update
 
 
 def dir_path(string) -> Optional[pathlib.Path]:
@@ -28,10 +28,28 @@ def optional_int(value):
     return None if value is None else int(value)
 
 
-parser = argparse.ArgumentParser(description="Updates all language files to a specific directory.")
+parser = argparse.ArgumentParser(description="Updates language files to a destination directory.")
+
 parser.add_argument("dest", help="Destination directory.", type=dir_path, default=None, nargs="?")
-parser.add_argument("--start", help="Specify language index to start after.", type=int, default=0)
-parser.add_argument("--end", help="Specify language index to end on.", type=optional_int, default=None)
+
+parser.add_argument(
+    "--languages",
+    help="List of languages to update (defaults to languages.txt if not specified).",
+    default=[],
+    nargs="+"
+)
+
+parser.add_argument(
+    "--force-update",
+    help="Do not check previous update time, and overwrite existing destination directory if existent.",
+    action="store_true"
+)
+
+parser.add_argument(
+    "--suppress-logging",
+    help="Do not set logging level to 'logging.INFO'.",
+    action="store_true"
+)
 
 
 if __name__ == '__main__':
@@ -39,14 +57,21 @@ if __name__ == '__main__':
     var_args = vars(args)
 
     data_root_path = var_args["dest"]
-    start = var_args["start"]
-    end = var_args["end"]
 
-    logging.root.setLevel(logging.INFO)
-    updated_languages = update_all(data_root_path, start=start, end=end)
+    if not var_args["suppress_logging"]:
+        logging.root.setLevel(logging.INFO)
+
+    if not var_args["languages"]:
+        languages_path = pathlib.Path(__file__).parent.joinpath("languages.txt")
+        with open(languages_path, 'r') as f:
+            languages = [s.strip() for s in f.readlines()]
+    else:
+        languages = var_args["languages"]
+
+    updated_languages = update(languages, root_path=data_root_path, force_update=var_args["force_update"])
 
     if data_root_path:
         data_root_path: pathlib.Path
 
         with open(data_root_path.joinpath("meta.json"), 'w') as f:
-            json.dump({"updated": str(datetime.date.today()), "updated_languages": updated_languages}, f, indent=1)
+            json.dump({"finished_run": datetime.datetime.now().isoformat(), "updated_languages": updated_languages}, f, indent=1)
